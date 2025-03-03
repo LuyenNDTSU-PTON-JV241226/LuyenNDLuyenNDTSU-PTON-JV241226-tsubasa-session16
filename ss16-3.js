3e1d3d2d24bcde339b7aef74e72cc9dd7377033e
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-03-03 23:51:43 +0900

    upload btvn ss16-3

---
+++ ss16-3.js
@@ -0,0 +1,8 @@
+function sum (a,b){
+    let result = a + b;
+    return result;
+};
+let a = +prompt("mời bạn nhập vào số thứ 1");
+let b = +prompt("mời bạn nhập vào số thứ 2");
+let c = sum(a, b);
+alert(c);
